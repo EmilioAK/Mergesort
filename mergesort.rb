96597276b4e0de7168c095ef2a00b40eb3d85336
EmilioAK <emilio.alvarez96@gmail.com>
2022-03-12 17:10:53 +0100

    Better version

--- mergesort.rb
+++ mergesort.rb
@@ -1,17 +1,17 @@
-def merge_sort(list)
-  return list if list.size < 2
+def merge(arr1, arr2, result = [])
+  if arr1.first > arr2.first
+    result << arr2.shift
+    return result + arr1 if arr2.empty?
+  else
+    result << arr1.shift
+    return result + arr2 if arr1.empty?
+  end
+  merge(arr1, arr2, result)
+end
 
-  first_half, second_half = split_list_in_two(list)
+def merge_sort(arr)
+  return arr if arr.size < 2
+
+  first_half, second_half = arr.each_slice((arr.size / 2.0).round).to_a
   merge(merge_sort(first_half), merge_sort(second_half))
-end
-
-def split_list_in_two(list)
-  [list[0...list.size / 2], list[list.size / 2..-1]]
-end
-
-def merge(l1, l2)
-  merged_list = []
-  merged_list << (l1[0] > l2[0] ? l2.shift : l1.shift) until l1.empty? || l2.empty?
-  l1.empty? ? merged_list.concat(l2) : merged_list.concat(l1)
-  merged_list
 end
